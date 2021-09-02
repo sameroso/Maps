97e58fde68c31dbec9f95a952095413b0880e7e4
Samer Kayali <samerkkr@gmail.com>
2021-09-01 22:21:18 -0300

    feat: added location ans popup to maps

---
+++ src/CustomMap.ts
@@ -0,0 +1,36 @@
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "<img src='https://img.ibxk.com.br/2021/07/15/15131120740312.jpg?w=704&h=264&mode=crop&scale=both'/>",
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world')
+/// <reference types="@types/google.maps" />
+
+import { CustomMap } from "./CustomMap";
+import { User } from "./User";
+import { Company } from "./Company";
+
+const customMap = new CustomMap("map");
+const company = new Company();
+const user = new User();
+
+customMap.addMarker(user);
+customMap.addMarker(company);
